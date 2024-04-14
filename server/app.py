008617d40d9cb0b5468657ec494fd61116d09080
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2024-04-14 10:18:10 -0400

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_compact = False
 
 migrate = Migrate(app, db)
 
@@ -21,11 +21,24 @@ api = Api(app)
 class ClearSession(Resource):
 
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session.pop('page_views', None)
+        session.pop('user_id', None)
 
         return {}, 204
+
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200
+        else:
+            return jsonify({'error': 'User not found'}), 404
 
 class IndexArticle(Resource):
     
@@ -49,9 +62,11 @@ class ShowArticle(Resource):
         return {'message': 'Maximum pageview limit reached'}, 401
 
 api.add_resource(ClearSession, '/clear')
+api.add_resource(Login, '/login')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
